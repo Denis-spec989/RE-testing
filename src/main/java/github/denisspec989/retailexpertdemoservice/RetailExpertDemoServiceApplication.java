de20ae213f3fd45bd652981ca7c1fa3b9e05db14
Denis Ignatov <dsignatov@licard.com>
2023-05-11 19:03:53 +0300

    feat: init project with base configs and starters

---
+++ src/main/java/github/denisspec989/retailexpertdemoservice/RetailExpertDemoServiceApplication.java
@@ -0,0 +1,13 @@
+package github.denisspec989.retailexpertdemoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailExpertDemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailExpertDemoServiceApplication.class, args);
+	}
+
+}
